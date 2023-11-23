b265718f73ab31d2f64879953d04c0d98db97211
maneth <manethgamage654@gmail.com>
2023-11-23 12:12:34 +0530

    buttons

--- src/main/java/com/example/oopp/ClubAdvisorController.java
+++ src/main/java/com/example/oopp/ClubAdvisorController.java
@@ -1,4 +1,68 @@
 package com.example.oopp;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.layout.AnchorPane;
+
 public class ClubAdvisorController {
+    @FXML
+    private AnchorPane AttendencePane;
+
+    @FXML
+    private AnchorPane JoinRequestsPane;
+
+    @FXML
+    private Button ManageClubButton;
+
+    @FXML
+    private Button SheduleEventsButton;
+
+    @FXML
+    private Button clubAttendenceButton;
+
+    @FXML
+    private Button clubJoinRequestButton;
+
+    @FXML
+    private AnchorPane createClubAnchorPane;
+
+    @FXML
+    private AnchorPane eventSchedulingPane;
+
+    @FXML
+    public void mainClick(ActionEvent event){
+        if (event.getSource()==ManageClubButton){
+            createClubAnchorPane.setVisible(true);
+            AttendencePane.setVisible(false);
+            eventSchedulingPane.setVisible(false);
+            JoinRequestsPane.setVisible(false);
+        }
+        if (event.getSource()==SheduleEventsButton){
+            eventSchedulingPane.setVisible(true);
+            createClubAnchorPane.setVisible(false);
+            JoinRequestsPane.setVisible(false);
+            AttendencePane.setVisible(false);
+
+        }
+        if (event.getSource()==clubJoinRequestButton) {
+            eventSchedulingPane.setVisible(false);
+            createClubAnchorPane.setVisible(false);
+            JoinRequestsPane.setVisible(true);
+            AttendencePane.setVisible(false);
+        }
+
+        if (event.getSource()==clubAttendenceButton){
+            eventSchedulingPane.setVisible(false);
+            createClubAnchorPane.setVisible(false);
+            JoinRequestsPane.setVisible(false);
+            AttendencePane.setVisible(true);
+        }
+
+
+
+
+
+    }
+
 }
