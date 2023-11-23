65eea432f563164bea87622233a359694c2f6ece
Benura Wickramanayake <benurawickramanayake255@gmail.com>
2023-11-23 14:13:08 +0530

    created Student class and StudentControoller class

--- src/main/java/com/example/oopp/Student.java
+++ src/main/java/com/example/oopp/Student.java
@@ -1,23 +1,51 @@
 package com.example.oopp;
 
-import javafx.application.Application;
-import javafx.fxml.FXMLLoader;
-import javafx.scene.Scene;
-import javafx.stage.Stage;
+import java.util.List;
+import java.util.SplittableRandom;
 
-import java.io.IOException;
+public class Student {
+    private String name;
+    private String email;
+    private String username;
+    private String password;
+    //private List<club>
 
-public class Student extends Application {
-    @Override
-    public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("student.fxml"));
-        Scene scene = new Scene(fxmlLoader.load());
-        stage.setTitle("Student Club Activity Management System");
-        stage.setScene(scene);
-        stage.show();
+    public Student(String name, String email, String username, String password) {
+        this.name = name;
+        this.email = email;
+        this.username = username;
+        this.password = password;
     }
 
-    public static void main(String[] args) {
-        launch();
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
     }
 }
