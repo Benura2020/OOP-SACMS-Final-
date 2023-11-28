c1a89042b22e03456dc6c6876116924a6609cf06
Benura Wickramanayake <benurawickramanayake255@gmail.com>
2023-11-28 18:12:09 +0530

    database connection

--- src/main/java/com/example/oopp/Club.java
+++ src/main/java/com/example/oopp/Club.java
@@ -4,6 +4,16 @@ public class Club {
     private String clubId;
     private String clubName;
 
+    public Club(String clubName){
+
+    }
+
+    public Club(String clubId, String clubName, String clubDescription) {
+        this.clubId = clubId;
+        this.clubName = clubName;
+        this.clubDescription = clubDescription;
+    }
+
     public String getClubDescription() {
         return clubDescription;
     }
@@ -14,39 +24,8 @@ public class Club {
 
     private String clubDescription;
 
-    public ClubAdvisor getClubAdvisor() {
-        return clubAdvisor;
-    }
-
-    public void setClubAdvisor(ClubAdvisor clubAdvisor) {
-        this.clubAdvisor = clubAdvisor;
-    }
-
-    private ClubAdvisor clubAdvisor;
-    public Club() {
-        this.clubId = clubId;
-        this.clubName = clubName;
-        this.clubDescription = clubDescription;
-    }
-
-    public Club(String clubName) {
-        this.clubName = clubName;
-    }
 
     public String getClubName() {
-        return clubName;
-    }
-
-    public void setClubName(String clubName) {
-        this.clubName = clubName;
-    }
-
-    public void setClubId(String clubId) {
-    }
-
-    public void setClubDescription(String clubDescription) {
-    }
-
-    public void setTeacherId(String teacherId) {
+        return null;
     }
 }
